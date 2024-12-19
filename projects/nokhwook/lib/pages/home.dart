4a6a88d9b76962628480769912a1cb4052c20277
ludak <ludak@ludak.me>
2024-12-19 22:00:56 +0400

    Address issues: Remove description from the tabs #9

--- projects/nokhwook/lib/pages/home.dart
+++ projects/nokhwook/lib/pages/home.dart
@@ -65,6 +65,7 @@ class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
         bottomNavigationBar: BottomNavigationBar(
           type: BottomNavigationBarType.fixed,
           showUnselectedLabels: false,
+          showSelectedLabels: false,
           currentIndex: selectedTab,
           onTap: switchTab,
           items: const [
